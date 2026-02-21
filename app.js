070c2cc64bcd56baebc22d9be17e87dff6075216
Maciej7JS <macieysee27@gmail.com>
2026-02-21 19:15:03 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+//let my_var = "666"
+//console.log(typeof(my_var))
+//console.log(typeof(Number(my_var)));
+
+
+//let isLoggedIn = true;
+//console.log(isLoggedIn)
+
+
+//const x = -5;
+//if (x>0) {
+  //  console.log("x jest dodatnie");
+//} else {
+//    console.log("x jest ujemne")
+//}
+
+
+//let isLoggedIn = true;
+//if (isLoggedIn) {
+ //   console.log("witaj");
+//} else {
+//    console.log("zaloguj się");
+//}
+
+
+//const names = ["Bob", "Ann", "Robert"];
+//names.push("David");
+//let tablica = [];
+//names.sort();
+//console.log(names);
+//console.log(names.length);
+//console.log(names.includes("David"));
+//console.log(names.includes("Adam"));
+
+
+//for (let i = 1; i <= 10; i++) {
+  //console.log(i, "Hello, World!");
+  //
+//}
+
+
+//PRACA DOMOWA Zad 1.
+const names = ["Bob", "Ann", "Robert", "Cristiano"];
+let tablica = [];
+console.log(names);
+console.log(names[3]);
+
+
+//PRACA DOMOWA Zad 3.
+for (let i = 1; i <= 20; i++, i++) {
+  console.log(i);
+
+}
+
+
+//PRACA DOMOWA Zad 2.
+const temperature = 16;
+if (temperature < 0) {
+  console.log("Mróz! Ubierz się ciepło!");
+} 
+else if (temperature > 15) {
+  console.log("Ciepło, zostaw kurtkę w domu");
+}
+else {
+  console.log("Chłodno. Przyda się kurtka");
+}
